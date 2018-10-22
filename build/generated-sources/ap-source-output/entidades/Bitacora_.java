d2f14921fc7673bf72bff55dd82beda12640bca8
Wilson Morales <wmorales_3@hotmail.com>
2018-10-22 14:31:23 -0600

    V 2.0.1

--- build/generated-sources/ap-source-output/entidades/Bitacora_.java
+++ build/generated-sources/ap-source-output/entidades/Bitacora_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-22T14:13:24")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-22T14:30:35")
 @StaticMetamodel(Bitacora.class)
 public class Bitacora_ { 
 
